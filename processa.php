eb3f5ba5f858112b09af651ff61d36bd40d8ee22
Caio Vinicius <caio.v.mangueira@gmail.com>
2025-04-22 16:39:22 -0300

    PhpTeste

--- processa.php
+++ processa.php
@@ -1,26 +1,40 @@
 <?php
+// filepath: c:\wamp64\www\AVA-\processa.php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Captura os dados do formulário
-    $nome = htmlspecialchars($_POST["nome"]);
-    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $mensagem = htmlspecialchars($_POST["mensagem"]);
+    $nome = isset($_POST["nome"]) ? htmlspecialchars($_POST["nome"]) : "";
+    $email = isset($_POST["email"]) ? filter_var($_POST["email"], FILTER_SANITIZE_EMAIL) : "";
+    $mensagem = isset($_POST["mensagem"]) ? htmlspecialchars($_POST["mensagem"]) : "";
 
-    // Configurações do e-mail
-    $para = "caio.vmangueira@gmail.com";  // Substitua pelo seu e-mail
-    $assunto = "Novo contato do site";
-    $corpo = "Você recebeu uma nova mensagem do formulário de contato:\n\n";
-    $corpo .= "Nome: $nome\n";
-    $corpo .= "E-mail: $email\n";
-    $corpo .= "Mensagem:\n$mensagem\n";
+    $mail = new PHPMailer(true);
 
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
+    try {
+       // Looking to send emails in production? Check out our Email API/SMTP product!
+    $phpmailer = new PHPMailer();
+    $phpmailer->isSMTP();
+    $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->Port = 2525;
+    $phpmailer->Username = '0874ec67a76f90';
+    $phpmailer->Password = '71392756a6ea5e';
 
-    // Envia o e-mail
-    if (mail($para, $assunto, $corpo, $headers)) {
+        // Configurações do e-mail
+        $mail->setFrom('seuemail@gmail.com', 'Seu Nome');
+        $mail->addAddress('caio.v.mangueira@gmail.com'); // Destinatário
+        $mail->Subject = 'Novo contato do site';
+        $mail->Body = "Você recebeu uma nova mensagem do formulário de contato:\n\n"
+                    . "Nome: $nome\n"
+                    . "E-mail: $email\n"
+                    . "Mensagem:\n$mensagem\n";
+
+        $mail->send();
         echo "Mensagem enviada com sucesso!";
-    } else {
-        echo "Erro ao enviar a mensagem. Tente novamente mais tarde.";
+    } catch (Exception $e) {
+        echo "Erro ao enviar a mensagem: {$mail->ErrorInfo}";
     }
 }
 ?>
